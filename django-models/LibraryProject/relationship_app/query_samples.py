fe0c2b5827b7303e1b90c0662dbe1d63739e9580
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-02-22 20:28:25 +0200

    query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,5 +2,5 @@ from .models import Author,Book,Library,Librarian
 
 
 books_by_author = Book.objects.filter(author__name="J.K. Rowling")
-books_in_library = Library.objects.get(name="Central Library").books.all()
+books_in_library = Library.objects.get(name=library_name)
 librarian_for_library = Library.objects.get(name="Central Library").librarian
