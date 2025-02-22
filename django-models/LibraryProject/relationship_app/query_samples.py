8a54ed2d7f81e9c906eda01b88333f57c679eac2
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-02-22 20:56:23 +0200

    query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,16 +3,17 @@ from .models import Author,Book,Library,Librarian
 
 
 def get_books_by_author(author_name):
-    try:
-        books = Book.objects.filter(author=Author.objects.get(name="J.K. Rowling"))
-    except Author.DoesNotExist:
-        return None
-def get_all_book_in_library(name_library):
-    try:
-        books_inlibrary = Library.objects.get(name=name_library).books.all()
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
 
-    except Librarian.DoesNotExist:
-        return None
+def get_all_book_in_library(name_library):
+        library = Library.objects.get(name_library=name_library)
+        books = library.books.all()
+        return books
+
 
 def get_librarian(name_libraian):
-    quary = Librarian.objects.get(name=name_libraian).librarian
+    library = Library.objects.get(library=name_libraian)
+    librarian = Librarian.objects.get(library=name_libraian)
+    return librarian
