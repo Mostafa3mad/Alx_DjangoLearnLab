81be83f1c904d7dc2672790e54832d0c6a0f0472
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-02-23 19:23:37 +0200

    3. Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import include, path
 from . import views
 from .views import LoginView, LogoutView
 from .views import list_books,LibraryDetailView
-from .views import admin_view
-from .views import librarian_view
-from .views import member_view
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
@@ -17,7 +17,6 @@ urlpatterns = [
 
     path("admin-view/", admin_view, name="admin_view"),
     path("librarian-view/", librarian_view, name="librarian_view"),
-    path("member-view/", member_view, name="member_view"),
-
+    path("member-view/", member_view, name="member_view")
 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,8 +6,7 @@ from .models import Library
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView as BaseLoginView, LogoutView as BaseLogoutView
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-from django.contrib.auth.decorators import login_required, user_passes_test
-from .models import UserProfile
+
 
 def list_books(request):
     books = Book.objects.all()  # Fetch all books
@@ -47,47 +46,3 @@ class LogoutView(BaseLogoutView):
 ##############################################################################
 
 
-def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == "Admin"
-@login_required
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, "relationship_app/admin_view.html", {"role": "Admin"})
-
-
-
-
-def is_librarian(user):
-    if not user.is_authenticated:
-        return False
-    try:
-        return user.userprofile.role == "Librarian"
-    except UserProfile.DoesNotExist:
-        return False
-
-
-
-@login_required
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, "relationship_app/librarian_view.html", {"role": "Librarian"})
-
-
-
-
-
-
-def is_member(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == "Member"
-
-@login_required
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, "relationship_app/member_view.html", {"role": "Member"})
-
-
-
-@login_required
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, "relationship_app/member_view.html", {"role": "Member"})
