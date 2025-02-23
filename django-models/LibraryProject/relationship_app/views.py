55961597836add1bd8ab95ecdcb4cd9bad86f8e6
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-02-23 17:47:33 +0200

    auth_login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Book
@@ -19,3 +19,45 @@ class LibraryDetailView(DetailView):
         return context
 
 
+
+
+#########################################################################
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after registration
+            return redirect("home")  # Redirect to home page
+    else:
+        form = UserCreationForm()
+
+    return render(request, "relationship_app/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")  # Redirect to home after login
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "relationship_app/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")  # Redirect to home after logout
+
+# Protected Home Page (only logged-in users can access)
+@login_required
+def home_view(request):
+    return render(request, "relationship_app/home.html")
