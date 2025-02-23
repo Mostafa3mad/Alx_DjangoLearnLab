5842a3576817d95f462e952c280db240a016147f
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-02-23 17:33:57 +0200

    view fiels

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,13 +5,12 @@ from .models import Book,Library,Librarian,Author
 
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(f"<pre>{book_list}</pre>")
+    books = Book.objects.all()  # Fetch all books
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/list_books.html"
     context_object_name = "library"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
