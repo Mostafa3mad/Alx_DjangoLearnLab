fd17966749b3eeede8ab25a7c652e3959900e8e2
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-02-23 17:48:14 +0200

    auth_login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,7 +22,7 @@ class LibraryDetailView(DetailView):
 
 
 #########################################################################
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
