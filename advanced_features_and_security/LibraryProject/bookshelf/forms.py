3c1722291777f3b4aec0811fb9fa6a7ce035b4c0
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-03-02 22:56:03 +0200

    Check if forms.py contains the definition of ExampleForm

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,13 @@
+# bookshelf/forms.py
+from django import forms
+from .models import Book  # استيراد نموذج الكتاب
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book  # ربط النموذج بـ Book
+        fields = ['title', 'author', 'description']  # الحقول التي سيتم عرضها في النموذج
+
+    # إضافة بعض التخصيصات إذا أردت
+    title = forms.CharField(max_length=200, required=True)
+    author = forms.CharField(max_length=100, required=True)
+    description = forms.CharField(widget=forms.Textarea, required=True)
