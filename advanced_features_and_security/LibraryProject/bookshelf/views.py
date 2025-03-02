6b7debd459f414c966a2fa0a209912096f834078
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-03-02 22:15:19 +0200

    1. Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .models import Book  # افترض أن لديك نموذجًا يسمى Book
 
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def edit_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == 'POST':
-        post.title = request.POST['title']
-        post.content = request.POST['content']
-        post.save()
-    return render(request, 'edit_post.html', {'post': post})
+@permission_required('bookshelf.can_view', raise_exception=True)  # التحقق من التصريح
+def book_list(request):
+    # جلب جميع الكتب من قاعدة البيانات
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
