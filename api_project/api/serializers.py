4c3c84d4fab4c826dd4d4c3310f7a2c467a3ad90
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-03-05 16:03:19 +0200

    add urls books

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,8 +1,7 @@
-# api/serializers.py
-from rest_framework import serializers  # Make sure to import serializers from DRF
-from .models import Book  # Import your Book model
+from rest_framework import serializers
+from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Book  # Specify the model to serialize
-        fields = '__all__'  # Include all fields of the Book model
+        model = Book
+        fields = '__all__'
