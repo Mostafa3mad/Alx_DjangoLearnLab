343ff43327aaeb502142420ff44268017e5e2109
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-03-16 05:50:20 +0200

    edit permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
 from .serializers import BookSerializer, AuthorSerializer
-from rest_framework.permissions  import IsAuthenticatedOrReadOnly,IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class BookListView(generics.ListAPIView):
