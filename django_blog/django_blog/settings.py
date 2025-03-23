c8e938da96f18ab92518804bb37622fda412a4fe
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-03-23 17:00:45 +0200

    add postgresql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'mostafa',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
