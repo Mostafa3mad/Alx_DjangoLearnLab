fe173f5dbe29d41b1f3e4fc760fda40369ff8a2e
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-03-30 17:14:30 +0200

    edit view

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -28,45 +28,56 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
-class FollowUserView(APIView):
-    """API to follow another user."""
+
+
+
+class FollowUserView(generics.GenericAPIView):
+    queryset = User.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(User, id=user_id)
+        user_to_follow = get_object_or_404(self.get_queryset(), id=user_id)
+
+        if user_to_follow == request.user:
+            return Response({'detail': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+
         if request.user.is_following(user_to_follow):
-            return Response({'detail': 'You are already following this user.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'detail': f'You are already following {user_to_follow.username}.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
         request.user.follow(user_to_follow)
         return Response({'detail': f'You are now following {user_to_follow.username}.'}, status=status.HTTP_200_OK)
 
 
-class UnfollowUserView(APIView):
-    """API to unfollow another user."""
+class UnfollowUserView(generics.GenericAPIView):
+    queryset = User.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(User, id=user_id)
+        user_to_unfollow = get_object_or_404(self.get_queryset(), id=user_id)
+
+        if user_to_unfollow == request.user:
+            return Response({'detail': 'You cannot unfollow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+
         if not request.user.is_following(user_to_unfollow):
-            return Response({'detail': 'You are not following this user.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'detail': f'You are not following {user_to_unfollow.username}.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
         request.user.unfollow(user_to_unfollow)
         return Response({'detail': f'You have unfollowed {user_to_unfollow.username}.'}, status=status.HTTP_200_OK)
 
 
 class FollowersListView(generics.ListAPIView):
-    """API to get the list of followers."""
     serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user
-        return user.followers.all()
+        return self.request.user.followers.all()
 
 
 class FollowingListView(generics.ListAPIView):
-    """API to get the list of users the authenticated user is following."""
     serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user
-        return user.following.all()
+        return self.request.user.following.all()
