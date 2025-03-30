818f3a135dd623c3a8053584b7b3beefdde66066
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2025-03-30 16:55:17 +0200

    add serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,16 @@
-from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
+from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from .models import User
+
+User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,20 +19,29 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        # Create user using get_user_model().objects.create_user()
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
             email=validated_data['email']
         )
+
+        # Create a token upon registration
+        Token.objects.create(user=user)
+
         return user
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
+        # Authenticate the user
         user = authenticate(**data)
         if user:
+            # Create or get the token if the user is authenticated
             token, created = Token.objects.get_or_create(user=user)
             return {'user': user, 'token': token.key}
+
         raise serializers.ValidationError('Incorrect username or password')
